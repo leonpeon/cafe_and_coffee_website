9bacb157a13b6255da59b36b85b570fc2c28f270
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-08-04 11:08:22 +1000

    Update website UI

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
 app = Flask(__name__)
+
+# Access DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
+db = SQLAlchemy(app)
+
+# Cafe class
+class Cafe(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
 @app.route("/")
 def home():
